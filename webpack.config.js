a106b8f62557339611361fbd0682ee7356e0f99c
Filippo Conti <filippo@codekraft.it>
2017-11-06 14:17:37 +0100

    Added more webpack loaders
    All the loaders relative to file/url/images loading are been added and configured

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,8 @@ const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const extractSass = new ExtractTextPlugin({
-  filename: `css/bundle.css`,
-  disable: process.env.NODE_ENV === "development"
+	filename: `css/bundle.css`,
+	disable: process.env.NODE_ENV === "development"
 })
 
 const src = path.resolve(__dirname, 'src')
@@ -18,127 +18,133 @@ const dist = path.resolve(__dirname, 'dist')
 
 module.exports = (env = {}) => {
 
-  return {
-    context: src,
-    entry: './index.js',
-    output: {
-      path: dist,
-      filename: `js/bundle.js`
-    },
-    devtool: env.development ? 'source-map' : false,
-    devServer: {
-      contentBase: src
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['es2015']
-            }
-          }
-        },
-        {
-          test: /\.(scss|sass)$/,
-          exclude: /node_modules/,
-          use: extractSass.extract({
-            fallback: "style-loader",
-            use: [
-              {
-                loader: "css-loader",
-                options: {
-                  sourceMap: env.development
-                }
-              },
-              {
-                loader: 'postcss-loader',
-                options: {
-                  sourceMap: env.development,
-                  config: {
-                    ctx: {
-                      autoprefixer: {}
-                    }
-                  }
-                }
-              },
-              {
-                loader: "sass-loader",
-                options: {
-                  sourceMap: env.development,
-                  outputStyle: env.development ? 'expanded' : 'compressed'
-                }
-              },
-            ]
-          })
-        }
-      ]
-    },
-    plugins: [
+	return {
+		context: src,
+		entry: './index.js',
+		output: {
+			path: dist,
+			filename: `js/bundle.js`
+		},
+		devtool: env.development
+			? 'source-map'
+			: false,
+		devServer: {
+			contentBase: src
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: ['es2015']
+						}
+					}
+				}, {
+					test: /\.(scss|sass)$/,
+					exclude: /node_modules/,
+					use: extractSass.extract({
+						fallback: "style-loader",
+						use: [
+							{
+								loader: "css-loader",
+								options: {
+									sourceMap: env.development
+								}
+							}, {
+								loader: 'resolve-url-loader'
+							}, {
+								loader: 'postcss-loader',
+								options: {
+									sourceMap: env.development,
+									config: {
+										ctx: {
+											autoprefixer: {}
+										}
+									}
+								}
+							}, {
+								loader: "sass-loader",
+								options: {
+									sourceMap: env.development,
+									outputStyle: env.development
+										? 'expanded'
+										: 'compressed'
+								}
+							}
+						]
+					})
+				}, {
+					test: /\.(png|jpg?g|gif|svg)$/,
+					use: [
+						{
+							loader: 'url-loader'
+						}, {
+							loader: 'img-loader'
+						}
+					]
+				}
+			]
+		},
+		plugins: [
 
-      // Recursive clean all files in dist directory
-      new CleanWebpackPlugin(['dist/**/*']),
+			// Recursive clean all files in dist directory
+			new CleanWebpackPlugin(['dist/**/*']),
 
-      // Uglify all the script files
-      new UglifyJSPlugin({
-        sourceMap: env.development
-      }),
+			// Uglify all the script files
+			new UglifyJSPlugin({sourceMap: env.development}),
 
-      // Extract the compiled css into separate file
-      extractSass,
+			// Extract the compiled css into separate file
+			extractSass,
 
-      // Add build banner
-      new webpack.BannerPlugin({
-        banner: `
-          Package: ${pkg.name} - v${pkg.version}
-          Description: ${pkg.description}
-          @author ${pkg.author}
-          @license ${pkg.license}
-        `,
-        entryOnly: true
-      }),
+			// Add build banner
+			new webpack.BannerPlugin({banner: `
+			Package: ${pkg.name} - v${pkg.version}
+			Description: ${pkg.description}
+			@author ${pkg.author}
+			@license ${pkg.license}
+			`, entryOnly: true}),
 
-      // Start browser sync server with proxy
-      new BrowserSyncPlugin({
-          host: 'localhost',
-          port: 8088,
-          proxy: 'http://localhost:8080/'
-        },
-        {
-          // prevent BrowserSync from reloading the page
-          // and let Webpack Dev Server take care of this
-          reload: false
-        }
-      ),
+			// Start browser sync server with proxy
+			new BrowserSyncPlugin({
+				host: 'localhost',
+				port: 8088,
+				proxy: 'http://localhost:8080/'
+			}, {
+				// prevent BrowserSync from reloading the page
+				// and let Webpack Dev Server take care of this
+				reload: false
+			}),
 
-      // Minify the main html entry
-      new HtmlWebpackPlugin({
-        inject: 'head',
-        template: './index.html',
-        minify: !env.development ? {
-          html5: true,
-          removeComments: true,
-          collapseWhitespace: true,
-          minifyCSS: true,
-          minifyJS: true,
-          removeEmptyElements: true,
-          cache: false
-        } : false
-      }),
+			// Minify the main html entry
+			new HtmlWebpackPlugin({
+				inject: 'head',
+				template: './index.html',
+				minify: !env.development
+					? {
+						html5: true,
+						removeComments: true,
+						collapseWhitespace: true,
+						minifyCSS: true,
+						minifyJS: true,
+						removeEmptyElements: true,
+						cache: false
+					}
+					: false
+			}),
 
-      // Copy source files into distribution folder
-      new CopyWebpackPlugin([
-        { from: './**/*' }
-      ], {
-        ignore: [
-          '*.js',
-          '*.scss'
-        ]
-      })
+			// Copy source files into distribution folder
+			new CopyWebpackPlugin([
+				{
+					from: './**/*'
+				}
+			], {
+				ignore: ['*.js', '*.scss']
+			})
 
-    ]
-  }
+		]
+	}
 
 };
